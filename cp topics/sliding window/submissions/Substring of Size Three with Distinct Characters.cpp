18d9f0fb4b2243cf63b4c09226bb80e026ee9985
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2021-11-06 10:43:04 +0530

    Create Substring of Size Three with Distinct Characters.cpp

---
+++ cp topics/sliding window/submissions/Substring of Size Three with Distinct Characters.cpp
@@ -0,0 +1,24 @@
+// problem Link : https://leetcode.com/tag/sliding-window/
+// Problem type: sliding window
+
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        if(s.size()<3) return 0;
+        int l,r;
+        l=0,r=2;
+        int goodStr=0;
+        unordered_map<char,int> ump;
+        ump[s[0]]++;
+        ump[s[1]]++;
+        ump[s[2]]++;
+        while(r<s.size()){
+            if(ump[s[l]]<2 && ump[s[l+1]]<2 && ump[s[l+2]]<2) goodStr++;
+            ump[s[l]]--;
+            r++;
+            l++;
+            if(r<s.size()) ump[s[r]]++;
+        }
+        return goodStr;
+    }
+};
