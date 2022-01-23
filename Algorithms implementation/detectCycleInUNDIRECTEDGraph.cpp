1e51f921a22f507e017cee1acb555e7824cdc475
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-23 11:17:57 +0530

    Update detectCycleInUNDIRECTEDGraph.cpp

--- Algorithms implementation/detectCycleInUNDIRECTEDGraph.cpp
+++ Algorithms implementation/detectCycleInUNDIRECTEDGraph.cpp
@@ -5,59 +5,47 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    // Function to detect cycle in a directed graph.
-    bool isCyclic1(vector<vector<int>> &adj,int V,int i,vector<bool>&recStack,vector<bool>&visited){
+    // Function to detect cycle in an undirected graph.
+    bool isCycle1(vector<vector<int>>& adj,int V,int i,vector<bool>&visited,int par=-1){
         if(i>=V) return false;
-        recStack[i]=true;
         visited[i]=true;
-        vector<int>& friends=adj[i];
-        for(auto node:friends){
-            if(!visited[node]){
-                if(isCyclic1(adj,V,node,recStack,visited)) return true;
-            }else if(recStack[node]){
-                return true;
-            }
+        for(auto node:adj[i]){
+            if(node == par) continue;
+            if(visited[node]) return true;
+            if(isCycle1(adj,V,node,visited,i)) return true;
         }
-        recStack[i]=false;
         return false;
     }
-    bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        vector<bool> recStack(V,0);
-        vector<bool> visited(V,0);
+    bool isCycle(int V, vector<int> adj[]) {
         vector<vector<int>> adj1(V);
+        for(int i=0;i<V;i++) adj1[i]=adj[i];
+        vector<bool> visited(V,0);
         for(int i=0;i<V;i++){
-            adj1[i]=adj[i];
-        }
-        for(int i=0;i<V;i++) 
-            if(!visited[i] && isCyclic1(adj1,V,i,recStack,visited)) 
-                return true;
-        return false;
+            if(!visited[i] && isCycle1(adj1,V,i,visited)) return true;
+        }return false;
     }
 };
 
 // { Driver Code Starts.
-
 int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
+    int tc;
+    cin >> tc;
+    while (tc--) {
         int V, E;
         cin >> V >> E;
-
         vector<int> adj[V];
-
         for (int i = 0; i < E; i++) {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-
         Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
     }
-
     return 0;
-}
-  // } Driver Code Ends
+}  // } Driver Code Ends
