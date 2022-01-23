679bf2d21b7d35c85b63fa74a348d32d0bf4ac74
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-23 11:18:17 +0530

    Update topoSortDFS.cpp

--- Algorithms implementation/topoSortDFS.cpp
+++ Algorithms implementation/topoSortDFS.cpp
@@ -3,61 +3,80 @@
 using namespace std;
 
  // } Driver Code Ends
-class Solution {
-  public:
-    // Function to detect cycle in a directed graph.
-    bool isCyclic1(vector<vector<int>> &adj,int V,int i,vector<bool>&recStack,vector<bool>&visited){
-        if(i>=V) return false;
-        recStack[i]=true;
-        visited[i]=true;
-        vector<int>& friends=adj[i];
-        for(auto node:friends){
-            if(!visited[node]){
-                if(isCyclic1(adj,V,node,recStack,visited)) return true;
-            }else if(recStack[node]){
-                return true;
-            }
-        }
-        recStack[i]=false;
-        return false;
-    }
-    bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        vector<bool> recStack(V,0);
-        vector<bool> visited(V,0);
-        vector<vector<int>> adj1(V);
-        for(int i=0;i<V;i++){
-            adj1[i]=adj[i];
-        }
-        for(int i=0;i<V;i++) 
-            if(!visited[i] && isCyclic1(adj1,V,i,recStack,visited)) 
-                return true;
-        return false;
-    }
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	void doTopoSort(vector<vector<int>>& adj,int V,int i,vector<int>&v,vector<bool>& visited){
+	    if(visited[i]) return ;
+	    if(i>=V) return;
+	    visited[i]=true;
+	    for(auto node:adj[i]){
+	        doTopoSort(adj,V,node,v,visited);
+	    }
+	    v.push_back(i);
+	    return;
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<vector<int>> adj1(V);
+	    for(int i=0;i<V;i++){
+	        adj1[i]=adj[i];
+	    }
+	    vector<int> v;
+	    vector<bool> visited(V,0);
+	    for(int i=0;i<V;i++) doTopoSort(adj1,V,i,v,visited);
+	    reverse(v.begin(),v.end());
+	    return v;
+	    // code here
+	}
 };
 
 // { Driver Code Starts.
 
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
 int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<int> adj[V];
+        vector<int> adj[N];
 
         for (int i = 0; i < E; i++) {
-            int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
         }
-
+        
         Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
-    }
+        vector <int> res = obj.topoSort(N, adj);
 
+        cout << check(N, res, adj) << endl;
+    }
+    
     return 0;
-}
-  // } Driver Code Ends
+}  // } Driver Code Ends
