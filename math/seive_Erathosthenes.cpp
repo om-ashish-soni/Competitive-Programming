734f4c5abb63d23479128d6880e337fa76cb1f29
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-26 19:11:12 +0530

    Update seive_Erathosthenes.cpp

--- math/seive_Erathosthenes.cpp
+++ math/seive_Erathosthenes.cpp
@@ -1,3 +1,58 @@
+// primeDivisors return type number
+int primeFactorsBig(int n){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	return uns.size();
+}
+// primeDivisors with return type vector
+void primeFactorsBig(int n,vector<int>& v){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	v.resize(uns.size());
+	int len=0;
+	for(auto num:uns) v[len++]=num;
+	sort(v.begin(), v.end());
+	return;
+}
+// isPrime
+bool isPrimeBig(int n){
+	if(n==1) return false;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				return false;
+				n/=p;
+			}else p++;
+		}else{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
 // sieve of erathosthens for prime
 const int N=1e6;
 vector<int32_t> highestPrime(1e6,0);
