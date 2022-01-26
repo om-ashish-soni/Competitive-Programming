978b18dacfe93f4dc4c2dc78108df2bed2ca57c3
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-26 18:15:19 +0530

    Create seive_Erathosthenes.cpp

---
+++ math/seive_Erathosthenes.cpp
@@ -0,0 +1,15 @@
+const int N=1e7;
+bitset <N> nonPrimeSet;
+
+void seive(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	
+	for(int num=2;num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num+num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+			}
+		}
+	}
+	
+}
