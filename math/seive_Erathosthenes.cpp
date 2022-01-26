8a3e8e5d2fb9cffda74978162bb8169b3de4cece
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-26 18:25:16 +0530

    Update seive_Erathosthenes.cpp

--- math/seive_Erathosthenes.cpp
+++ math/seive_Erathosthenes.cpp
@@ -1,10 +1,28 @@
-const int N=1e7;
+// sieve of erathosthens for prime
+const int N=1e6;
 bitset <N> nonPrimeSet;
-// TIME COMPLEXITY : N*LOG(N)
+// isPrime
+bool isPrimeBig(int n){
+	if(n==1) return false;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				return false;
+				n/=p;
+			}else p++;
+		}else{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+// TIME COMPLEXITY : NLOGN
 void seive(){
 	nonPrimeSet[0]=nonPrimeSet[1]=1;
 	
-	for(int num=2;num<=N;num++){
+	for(int num=2;num*num<=N;num++){
 		if(0==nonPrimeSet[num]){
 			for(int j=num+num;j<=N;j+=num){
 				nonPrimeSet[j]=1;
@@ -12,4 +30,8 @@ void seive(){
 		}
 	}
 	
+}
+bool isPrime(int n){
+	if(n>1e6) return isPrimeBig(n);
+	return 0==nonPrimeSet[n];
 }
