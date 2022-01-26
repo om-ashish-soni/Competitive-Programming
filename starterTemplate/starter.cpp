62317d88caceb66b6a1c2c5979551f7deb71cbbd
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-26 09:44:57 +0530

    Update starter.cpp

--- starterTemplate/starter.cpp
+++ starterTemplate/starter.cpp
@@ -2,6 +2,50 @@
 #define int long long
 #define endl "\n"
 using namespace std;
+// check timeout
+void checkTimeout(){
+	double tm=(float)clock()/CLOCKS_PER_SEC;
+	if(tm>=2){
+		cerr<<"Time limit exeeded"<<endl;
+		exit(0);
+	}
+}
+//factors 
+void findAllFactors(int n,std::vector<int>& factors){
+	double sqroot=sqrt(n);
+	factors.push_back(1);
+	for(int i=2;i<=sqroot;i++){
+		if(n%i == 0){
+			factors.push_back(i);
+			if(n/i != i){
+				factors.push_back(n/i);
+			}
+		}
+	}
+	factors.push_back(n);
+	sort(factors.begin(),factors.end());
+}
+//lcm and gcd
+int lcm(int a,int b){
+	return (a*b)/(__gcd(a,b));
+}
+int gcd(int a,int b){
+	return __gcd(a,b);
+}
+//printing modules
+void print(int num){
+	cout<<num<<endl;
+}
+void print(int *arr,int start,int end){
+	for(int i=start;i<=end;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void print(int *arr,int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
 void print(vector<pair<int,int>>& v){
 	for(auto &pr:v){
 		cout<<"{"<<pr.first<<","<<pr.second<<"}"<<endl;
@@ -15,21 +59,9 @@ void print(std::vector<string>& v){
 	for(auto &num:v) cout<<num<<" ";cout<<endl;
 }
 void print(std::vector<std::vector<int>>& v,int n){
-	// cout<<v.size()<<endl;
 	for(int i=n-1;i>=0;i--){
 		print(v[i]);
 	}
-	// for(auto &row: v) print(row);
-	// for(int i=v.size()-1;i>=0;i--){
-	// 	std::vector<int>& row=v[i];
-	// 	print(row);
-	// }
-}
-int lcm(int a,int b){
-	return (a*b)/(__gcd(a,b));
-}
-int gcd(int a,int b){
-	return __gcd(a,b);
 }
 void print(bool b){
 	if(b) cout<<"YES";
@@ -39,6 +71,8 @@ void print(bool b){
 void print(pair<int,int>& pr){
 	cout<<"{"<<pr.first<<","<<pr.second<<"} ";
 }
+
+// modulo arithmetic 
 int power(long long x, unsigned int y, int p) 
 { 
     int res = 1; 
@@ -57,28 +91,7 @@ int modMul(int a,int b,int mod){
 int modAdd(int a,int b,int mod){
 	return ((a%mod)+(b%mod))%mod;
 }
-void print(int *arr,int start,int end){
-	for(int i=start;i<=end;i++){
-		cout<<arr[i]<<" ";
-	}cout<<endl;
-}
-void print(int *arr,int n){
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}cout<<endl;
-}
-
-int recurs(vector<int>& v,int sz,int i,vector<int>& memo){
-	if(memo[i]) return memo[i];
-	int sum=1;
-	for(int j=i+1;j<sz;j++){
-		if(v[i]<=v[j]){
-			sum=max(sum,1+recurs(v,sz,j,memo));
-		}
-	}
-	memo[i]=sum;
-	return sum;
-}
+// fenwick tree
 void update(int *BIT,int n,int x, int delta)
 {
       for(; x <= n; x += x&-x)
@@ -91,16 +104,18 @@ int query(int *BIT,int x)
         sum += BIT[x];
      return sum;
 }
-int lengthOfLIS(vector<int>& nums) {
-        vector<int> memo;
-        for(auto num:nums){
-            auto found=lower_bound(memo.begin(),memo.end(),num);
-            if(found==memo.end()) memo.push_back(num);
-            else *found=num;
-        }
-        return memo.size();
-    }
 
+//lis
+int lis(int *v,int n){
+	vector<int> seq;
+	for(int i=0;i<n;i++){
+		int num=v[i];
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return seq.size();
+}
 int lis(vector<int>&v){
 	int n=v.size();
 	vector<int> seq;
@@ -111,6 +126,27 @@ int lis(vector<int>&v){
 	}
 	return seq.size();
 }
+//lis return type list
+void lis(int *v,int n,vector<int>&seq){
+	for(int i=0;i<n;i++){
+		int num=v[i];
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return ;
+}
+void lis(vector<int>&v,vector<int>& seq){
+	int n=v.size();
+	for(auto num:v){
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return;
+}
+
+// scanning modules
 void scan(int *arr,int n){
 	for(int i=0;i<n;i++) cin>>arr[i];
 }
@@ -129,43 +165,72 @@ void scan(int *arr,int n,unordered_map<int,int>& hash){
 void scan(vector<int>& v,int n,unordered_map<int,int>& hash){
 	for(int i=0;i<n;i++) {cin>>v[i];hash[v[i]]=i;}
 }
+// primeDivisors return type number
+int primeDivisors(int n){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	return uns.size();
+}
+// primeDivisors with return type vector
+void primeDivisors(int n,vector<int>& v){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	v.resize(uns.size());
+	int len=0;
+	for(auto num:uns) v[len++]=num;
+	sort(v.begin(), v.end());
+	return;
+}
+// isPrime
+bool isPrime(int n){
+	if(n==1) return false;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				return false;
+				n/=p;
+			}else p++;
+		}else{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+// roundOf
+int roundOf(int n){
+	return (int)pow(2,floor(log2(n)));
+}
 void solve(){
-	int n;
-	cin>>n;
-	int arr[n];
-	vector<int> hash(n+1);
-	vector<bool> tenfree(n+1,false);
-	scan(arr,n,hash);
-	int BIT[n+2];
-	memset(BIT,0,sizeof(BIT));
-	int mn=n;
-	int ctr=1;
-	while(mn>0){
-		int dest=hash[ctr];
-		// cout<<ctr<<" "<<dest<<endl;
-		int upperFree=query(BIT,n)-query(BIT,ctr);
-		if(upperFree>0){
-			tenfree[ctr]=true;
-		}
-		// cout<<"upperFree : "<<upperFree<<endl;
-		if(dest<mn){
-			for(int i=dest+1;i<mn;i++){
-				if(arr[i]<ctr) continue;
-				update(BIT,n,arr[i],1);
-				// cout<<"tenfree["<<arr[i]<<"] : true"<<endl;
-				tenfree[arr[i]]=true;
-			}
-			mn=dest;
-			//do some code here
-		}
-		
-		ctr++;
-	}
-	for(int i=2;i<=n;i++){
-		if(tenfree[i]);
-		else return print(false);
-	}
-	return print(true);
+	// int n;
+	// cin>>n;
+	// int arr[n];
+	// memset(arr,0,sizeof(arr));
+	// memset(arr,-1,sizeof(arr));
+	
 }
 
 signed main(){
