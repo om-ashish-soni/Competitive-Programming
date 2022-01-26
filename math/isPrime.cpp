3f9559a2d646369a394dc7114d4d02b32cd4ce9b
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-26 19:10:18 +0530

    Update isPrime.cpp

--- math/isPrime.cpp
+++ math/isPrime.cpp
@@ -1,24 +1,8 @@
 // sieve of erathosthens for prime
 const int N=1e6;
+vector<int32_t> highestPrime(1e6,0);
+vector<int32_t> lowestPrime(1e6,0);
 bitset <N> nonPrimeSet;
-// isPrime
-bool isPrimeBig(int n){
-	if(n==1) return false;
-	int p=2;
-	while(true){
-		if(n>=p*p){
-			if(n%p==0){
-				return false;
-				n/=p;
-			}else p++;
-		}else{
-			return true;
-			break;
-		}
-	}
-	return false;
-}
-// TIME COMPLEXITY : NLOGN
 void seive(){
 	nonPrimeSet[0]=nonPrimeSet[1]=1;
 	
@@ -31,7 +15,47 @@ void seive(){
 	}
 	
 }
+void seiveWithHPLP(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	
+	for(int num=2;num*num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num+num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+				if(lowestPrime[j]==0) lowestPrime[j]=num;
+				highestPrime[j]=num;
+			}
+			highestPrime[num]=lowestPrime[num]=num;
+		}
+	}
+}
 bool isPrime(int n){
 	if(n>1e6) return isPrimeBig(n);
 	return 0==nonPrimeSet[n];
+}
+void primeFactors(int n,vector<int>&v){
+	if(n<2) return;
+	if(n>1e6) return primeFactorsBig(n,v);
+	while(n>1){
+		int prime_factor=highestPrime[n];
+		while(n%prime_factor==0){
+			n/=prime_factor;
+			
+		}
+		v.push_back(prime_factor);
+	}
+}
+
+int primeFactors(int n){
+	if(n<2) return 0;
+	if(n>1e6) return primeFactorsBig(n);
+	int noOfPrimeFactors=0;
+	while(n>1){
+		int prime_factor=highestPrime[n];
+		while(n%prime_factor==0){
+			n/=prime_factor;
+			noOfPrimeFactors++;
+		}
+	}
+	return noOfPrimeFactors;
 }
