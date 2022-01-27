f21b7474c477129d26444c3f9a7cf91120548632
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-27 09:09:21 +0530

    Update isPrime.cpp

--- math/isPrime.cpp
+++ math/isPrime.cpp
@@ -71,8 +71,9 @@ bool isPrimeBig(int n){
 }
 // sieve of erathosthens for prime
 const int N=1e6;
-vector<int32_t> highestPrime(1e6,0);
-vector<int32_t> lowestPrime(1e6,0);
+vector<int> highestPrime(1e6,0);
+vector<int> lowestPrime(1e6,0);
+vector<int> primes;
 // vector<vector<int>> factorsOf(N);
 bitset <N> nonPrimeSet;
 void seive(){
@@ -80,19 +81,32 @@ void seive(){
 	
 	for(int num=2;num*num<=N;num++){
 		if(0==nonPrimeSet[num]){
-			for(int j=num+num;j<=N;j+=num){
+			for(int j=num*num;j<=N;j+=num){
 				nonPrimeSet[j]=1;
 			}
 		}
 	}
 	
 }
-void seiveWithHPLP(){
+void seiveWithAllPrimes(){
 	nonPrimeSet[0]=nonPrimeSet[1]=1;
 	
 	for(int num=2;num*num<=N;num++){
 		if(0==nonPrimeSet[num]){
-			for(int j=num+num;j<=N;j+=num){
+			for(int j=num*num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+			}
+		}
+	}
+	for(int num=2;num<=N;num++){
+		if(0==nonPrimeSet[num]) primes.push_back(num);
+	}
+}
+void seiveWithHPLP(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	for(int num=2;num*num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num*num;j<=N;j+=num){
 				nonPrimeSet[j]=1;
 				if(lowestPrime[j]==0) lowestPrime[j]=num;
 				highestPrime[j]=num;
@@ -100,6 +114,7 @@ void seiveWithHPLP(){
 			highestPrime[num]=lowestPrime[num]=num;
 		}
 	}
+	
 }
 // void seiveWithAllFactors(){
 // 	nonPrimeSet[0]=nonPrimeSet[1]=1;
@@ -107,12 +122,12 @@ void seiveWithHPLP(){
 // 	for(int num=2;num<=N;num++){
 // 		factorsOf[num].push_back(1);
 // 		if(0==nonPrimeSet[num]){
-// 			for(int j=num+num;j<=N;j+=num){
+// 			for(int j=num*num;j<=N;j+=num){
 // 				factorsOf[j].push_back(num);
 // 				nonPrimeSet[j]=1;
 // 				if(lowestPrime[j]==0) lowestPrime[j]=num;
 // 				highestPrime[j]=num;
-// 			}
+// 			}primes.push_back(num);
 // 			highestPrime[num]=lowestPrime[num]=num;
 // 		}else{
 // 			for(int j=num+num;j<=N;j+=num){
@@ -122,6 +137,46 @@ void seiveWithHPLP(){
 // 		factorsOf[num].push_back(num);
 // 	}
 // }
+
+void segmentedSeive(int l,int r,vector<int>& segPrimes){
+	int limit=ceil(sqrt(r));
+	int sz=r-l+1;
+	vector<bool> bs(sz,0);
+	for(auto primeNum:primes){
+		if(primeNum<=limit){
+			int firstMultiple=(l/primeNum)*primeNum;
+			if(firstMultiple<l) firstMultiple+=l;
+			for(int num=max(firstMultiple,primeNum*primeNum);num<=r;num+=primeNum){
+				bs[num-l]=1;
+			}
+		}
+	}
+	for(int i=0;i<sz;i++){
+		if(bs[i]==0) segPrimes.push_back(i+l);
+	}
+}
+int rangePrimes(int l,int r){
+	if(r<=1e6){
+		int ctr=0;
+		for(int i=l;i<=r;i++){
+			if(nonPrimeSet[i]==0) ctr++;
+		}
+		return ctr;
+	}else{
+		vector<int> segPrimes;
+		segmentedSeive(l,r,segPrimes);
+		return segPrimes.size();
+	}
+}
+void rangePrimes(int l,int r,vector<int>& segPrimes){
+	if(r<=1e6){
+		for(int i=l;i<=r;i++){
+			if(nonPrimeSet[i]==0) segPrimes.push_back(i);
+		}
+		return; 
+	}
+	return segmentedSeive(l,r,segPrimes);
+}
 bool isPrime(int n){
 	if(n>1e6) return isPrimeBig(n);
 	return 0==nonPrimeSet[n];
