e87e63459365269f27fa4fbb973717ed4d0ed03e
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-29 16:51:33 +0530

    Update starter.cpp

--- starterTemplate/starter.cpp
+++ starterTemplate/starter.cpp
@@ -234,6 +234,47 @@ class segmentTree{
 		}
 	}
 };
+// sqroot decomposition
+
+class sqrtDecomp{
+	int *arr;
+	int n;
+	int len;
+	vector<int> decomp;
+	public :
+	sqrtDecomp(int *arr,int n){
+		this->n=n;
+		this->arr=arr;
+		len=sqrt(n)+1;
+		decomp=*new vector<int>(len,0);
+		for(int i=0;i<n;i++){
+			decomp[i/len]+=arr[i];
+		}
+	}
+	sqrtDecomp(vector<int>& v){
+		this->n=n;
+		arr=(int *)malloc(n*sizeof(int));
+		for(int i=0;i<n;i++) arr[i]=v[i];
+		len=sqrt(n)+1;
+		decomp=*new vector<int>(len,0);
+		for(int i=0;i<n;i++){
+			decomp[i/len]+=arr[i];
+		}
+	}
+	int getRangeSum(int l,int r){
+		int sum=0;
+		for(int i=l;i<=r;){
+			if(i%len==0 && i+len-1<=r){
+				sum+=decomp[i/len];
+				i+=len;
+			}else{ 
+				sum+=arr[i];
+				i++;
+			}
+		}
+		return sum;
+	}
+};
 // disjointSet
 class disjointSet{
     unordered_map<int,int> ump;
@@ -676,14 +717,10 @@ void kmp(string s,string pattern,vector<int>&match){
 	}
 }
 void solve(){
-	int arr[] = {1, 3, 5, 7, 9, 12};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    vector<int> v(n);
-    for(int i=0;i<n;i++){
-    	v[i]=arr[i];
-    }
-    fenwickTree& fenTree=*new fenwickTree(arr,n);
-    cout<<fenTree.get(5)<<endl;
+	int arr[7]={1,2,3,4,5,6,7};
+	int n=7;
+	sqrtDecomp sd=*new sqrtDecomp(arr,n);
+	cout<<sd.getRangeSum(0,4)<<endl;
     
 }
 
