da4dac226e7ff897e04ff7482f1cbce3ee416d5f
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-29 15:54:37 +0530

    Update starter.cpp

--- starterTemplate/starter.cpp
+++ starterTemplate/starter.cpp
@@ -234,7 +234,51 @@ class segmentTree{
 		}
 	}
 };
-
+// disjointSet
+class disjointSet{
+    unordered_map<int,int> ump;
+    public:
+    disjointSet(){}
+    disjointSet(vector<int>& arr){
+        for(auto num:arr) ump[num]=-1;
+    }
+    int findSet(int u){
+        // cout<<"in findSet "<<u<<endl;
+        int r=u;
+        while(ump[r]>=0){
+            r=ump[r];
+        }
+        while(u!=r){
+            int par=ump[u];
+            ump[u]=r;
+            u=par;
+        }
+        return r;
+    }
+    bool setUnion(int u,int v){
+        // cout<<"in setUnion "<<u<<" "<<v<<endl;
+        int uroot=findSet(u);
+        int vroot=findSet(v);
+        if(uroot == vroot) return false;
+        int uchild=ump[uroot];
+        int vchild=ump[vroot];
+        int totalChild=uchild+vchild;
+        
+        if(uchild>vchild){
+            ump[uroot]=vroot;
+            ump[vroot]=totalChild;
+        }else{
+            ump[vroot]=uroot;
+            ump[uroot]=totalChild;
+        }
+        return true;
+    }
+    void print(){
+        for(auto &pr:ump){
+            cout<<"{"<<pr.first<<"<="<<pr.second<<"}"<<endl;
+        }
+    }
+};
 //lis
 int lis(int *v,int n){
 	vector<int> seq;
