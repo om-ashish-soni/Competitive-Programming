cce5a71c0b87b0fcd49d66505be8336160a1eb25
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-30 13:08:23 +0530

    Create advanceStarterTemplate.cpp

---
+++ starterTemplate/advanceStarterTemplate.cpp
@@ -0,0 +1,847 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+// check timeout
+void checkTimeout(){
+	double tm=(float)clock()/CLOCKS_PER_SEC;
+	if(tm>=2){
+		cerr<<"Time limit exeeded"<<endl;
+		exit(0);
+	}
+}
+//factors 
+void findAllFactors(int n,std::vector<int>& factors){
+	double sqroot=sqrt(n);
+	factors.push_back(1);
+	for(int i=2;i<=sqroot;i++){
+		if(n%i == 0){
+			factors.push_back(i);
+			if(n/i != i){
+				factors.push_back(n/i);
+			}
+		}
+	}
+	factors.push_back(n);
+	sort(factors.begin(),factors.end());
+}
+//lcm and gcd
+pair<int,int> extended_gcd(int a,int b){
+	if(b==0) return{1,1};
+	pair<int,int> pr=extended_gcd(b,a%b);
+	return {pr.second,pr.first-(a/b)*pr.second};
+}
+int hcf(int a,int b){
+	if(a==0 || b==0) return max(a,b);
+	pair<int,int> pr=extended_gcd(a,b);
+	return (a*pr.first + b*pr.second);
+}
+int lcm(int a,int b){
+	return (a*b)/(__gcd(a,b));
+}
+int gcd(int a,int b){
+	return __gcd(a,b);
+}
+//printing modules
+void print(int num){
+	cout<<num<<endl;
+}
+void print(int *arr,int start,int end){
+	for(int i=start;i<=end;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void print(int *arr,int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void print(vector<pair<int,int>>& v){
+	for(auto &pr:v){
+		cout<<"{"<<pr.first<<","<<pr.second<<"}"<<endl;
+	}
+	cout<<endl;
+}
+void print(std::vector<int>& v){
+	for(auto &num:v) cout<<num<<" ";cout<<endl;
+}
+void print(std::vector<string>& v){
+	for(auto &num:v) cout<<num<<" ";cout<<endl;
+}
+void print(std::vector<std::vector<int>>& v,int n){
+	for(int i=n-1;i>=0;i--){
+		print(v[i]);
+	}
+}
+void print(bool b){
+	if(b) cout<<"YES";
+	else cout<<"NO";
+	cout<<endl;
+}
+void print(pair<int,int>& pr){
+	cout<<"{"<<pr.first<<","<<pr.second<<"} ";
+}
+
+// modulo arithmetic 
+int power(long long x, unsigned int y, int p) 
+{ 
+    int res = 1; 
+    x = x % p; // Update x if it is more than or 
+    if (x == 0) return 0; // In case x is divisible by p;
+    while (y > 0) { 
+        if (y & 1) res = (res*x) % p; 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    return res; 
+} 
+int modMul(int a,int b,int mod){
+	return ((a%mod)*(b%mod))%mod;
+}
+int modAdd(int a,int b,int mod){
+	return ((a%mod)+(b%mod))%mod;
+}
+int modInv(int a,int mod){
+	int x,y;
+	pair<int,int> pr=extended_gcd(a,mod);
+	x=pr.first;
+	y=pr.second;
+	int g=(a*x + mod*y);
+	if(g!=1){
+		cerr<<"modular inverse does not exist , gcd != 1"<<endl;
+		exit(0);
+	}
+	// adding m to avoid negative value of x 
+	int res=(x%mod + mod)%mod;
+	return res;
+}
+int modDiv(int a,int b,int mod){
+	
+	a=a%mod;
+	int inv=modInv(b,mod);
+	int res=(inv*a)%mod;
+	return res;
+}
+int chineseRem(int arr[],int rem[],int n){
+	int prod=1,res=0;
+	for(int i=0;i<n;i++) prod*=arr[i];
+	for(int i=0;i<n;i++){
+		int restProd=prod/arr[i];
+		res+=(rem[i]*modInv(restProd,arr[i])*(restProd));
+	}
+	return (res%prod);
+}
+// fenwick tree
+class fenwickTree{
+	vector<int> BIT;
+	int n;
+	public : 
+	fenwickTree(int *arr,int n){
+		
+		this->n=n;
+		BIT=*new vector<int>(n,0);
+		BIT[0]=arr[0];
+		for(int i=1;i<n;i++){
+			this->update(i,arr[i]);
+		}
+	}
+	fenwickTree(vector<int>&v){
+		this->n=n;
+		BIT=*new vector<int>(n,0);
+		BIT[0]=v[0];
+		for(int i=1;i<n;i++){
+			this->update(i,v[i]);
+		}
+	}
+	void update(int index, int delta)
+	{
+	      for(; index < n; index += index&-index)
+	        BIT[index] += delta;
+	}
+	int get(int index)
+	{
+	     int sum = BIT[0];
+	     for(; index > 0; index -= index&-index)
+	        sum += BIT[index];
+	     return sum;
+	}
+};
+	
+//segment tree
+class segmentTree{
+	vector<int> segTree;
+	int *arr;int n;
+	bool isVectorContainer=false;
+	public: 
+	segmentTree(int *arr,int n){
+		this->arr=arr;
+		this->n=n;
+		int size=pow(2,ceil(log2(2*n-1)));
+		segTree.resize(size);
+		createSegTree(0,0,n-1);
+	}
+	segmentTree(vector<int> &v){
+		isVectorContainer=true;
+		n=v.size();
+		this->arr=(int *)malloc(n*sizeof(int));
+		for(int i=0;i<n;i++){
+			arr[i]=v[i];
+		}
+		int size=pow(2,ceil(log2(2*n-1)));
+		segTree.resize(size);
+		createSegTree(0,0,n-1);
+	}
+	int createSegTree(int si,int l,int r){
+		if(l==r){	
+			segTree[si]=arr[l];
+			return arr[l];
+		}
+		int mid=(l+r)/2;
+		segTree[si]=createSegTree(2*si + 1,l,mid)+createSegTree(2*si + 2,mid+1,r);
+		return segTree[si];
+	}
+	int getRangeSum(int sl,int sr,int l,int r,int si){
+		// cout<<sl<<" "<<sr<<endl;
+		if(sl>=l && sr<=r){
+			return segTree[si];
+		}
+		if(sl>r || sr<l){
+			return 0;
+		}
+		int mid=(sl+sr)/2;
+		return getRangeSum(sl,mid,l,r,2*si+1)+getRangeSum(mid+1,sr,l,r,2*si+2);
+	}
+	int getRangeSum(int l,int r){
+		return this->getRangeSum(0,n-1,l,r,0);
+	}
+
+	void updateSegTree(int si,int sl,int sr,int pos,int diff){
+		if(sl>pos || sr<pos) return;
+		segTree[si]+=diff;
+		if(sl != sr){
+			int mid((sl+sr)/2);
+			updateSegTree(2*si+1,sl,mid,pos,diff);
+			updateSegTree(2*si+2,mid+1,sr,pos,diff);
+		}
+	}
+	void update(int pos,int x){
+		int diff=x-arr[pos];
+		return updateSegTree(0,0,n-1,pos,diff);
+	}
+	~segmentTree(){
+		if(isVectorContainer){
+			free(arr);
+		}
+	}
+};
+// sqroot decomposition
+
+class sqrtDecomp{
+	int *arr;
+	int n;
+	int len;
+	vector<int> decomp;
+	public :
+	sqrtDecomp(int *arr,int n){
+		this->n=n;
+		this->arr=arr;
+		len=sqrt(n)+1;
+		decomp=*new vector<int>(len,0);
+		for(int i=0;i<n;i++){
+			decomp[i/len]+=arr[i];
+		}
+	}
+	sqrtDecomp(vector<int>& v){
+		this->n=n;
+		arr=(int *)malloc(n*sizeof(int));
+		for(int i=0;i<n;i++) arr[i]=v[i];
+		len=sqrt(n)+1;
+		decomp=*new vector<int>(len,0);
+		for(int i=0;i<n;i++){
+			decomp[i/len]+=arr[i];
+		}
+	}
+	int getRangeSum(int l,int r){
+		int sum=0;
+		for(int i=l;i<=r;){
+			if(i%len==0 && i+len-1<=r){
+				sum+=decomp[i/len];
+				i+=len;
+			}else{ 
+				sum+=arr[i];
+				i++;
+			}
+		}
+		return sum;
+	}
+};
+// disjointSet
+class disjointSet{
+    unordered_map<int,int> ump;
+    public:
+    disjointSet(){}
+    disjointSet(vector<int>& arr){
+        for(auto num:arr) ump[num]=-1;
+    }
+    int findSet(int u){
+        // cout<<"in findSet "<<u<<endl;
+        int r=u;
+        while(ump[r]>=0){
+            r=ump[r];
+        }
+        while(u!=r){
+            int par=ump[u];
+            ump[u]=r;
+            u=par;
+        }
+        return r;
+    }
+    bool setUnion(int u,int v){
+        // cout<<"in setUnion "<<u<<" "<<v<<endl;
+        int uroot=findSet(u);
+        int vroot=findSet(v);
+        if(uroot == vroot) return false;
+        int uchild=ump[uroot];
+        int vchild=ump[vroot];
+        int totalChild=uchild+vchild;
+        
+        if(uchild>vchild){
+            ump[uroot]=vroot;
+            ump[vroot]=totalChild;
+        }else{
+            ump[vroot]=uroot;
+            ump[uroot]=totalChild;
+        }
+        return true;
+    }
+    void print(){
+        for(auto &pr:ump){
+            cout<<"{"<<pr.first<<"<="<<pr.second<<"}"<<endl;
+        }
+    }
+};
+//lis
+int lis(int *v,int n){
+	vector<int> seq;
+	for(int i=0;i<n;i++){
+		int num=v[i];
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return seq.size();
+}
+int lis(vector<int>&v){
+	int n=v.size();
+	vector<int> seq;
+	for(auto num:v){
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return seq.size();
+}
+//lis return type list
+void lis(int *v,int n,vector<int>&seq){
+	for(int i=0;i<n;i++){
+		int num=v[i];
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return ;
+}
+void lis(vector<int>&v,vector<int>& seq){
+	int n=v.size();
+	for(auto num:v){
+		auto found=upper_bound(seq.begin(), seq.end(),num);
+		if(found==seq.end()) seq.push_back(num);
+		else *found=num;
+	}
+	return;
+}
+
+// scanning modules
+void scan(int *arr,int n){
+	for(int i=0;i<n;i++) cin>>arr[i];
+}
+void scan(vector<int>& v,int n){
+	for(int i=0;i<n;i++) cin>>v[i];
+}
+void scan(int *arr,int n,vector<int>& hash){
+	for(int i=0;i<n;i++) {cin>>arr[i];hash[arr[i]]=i;}
+}
+void scan(vector<int>& v,int n,vector<int>& hash){
+	for(int i=0;i<n;i++) {cin>>v[i];hash[v[i]]=i;}
+}
+void scan(int *arr,int n,unordered_map<int,int>& hash){
+	for(int i=0;i<n;i++) {cin>>arr[i];hash[arr[i]]=i;}
+}
+void scan(vector<int>& v,int n,unordered_map<int,int>& hash){
+	for(int i=0;i<n;i++) {cin>>v[i];hash[v[i]]=i;}
+}
+// primeDivisors return type number
+int primeFactorsBig(int n){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	return uns.size();
+}
+// primeDivisors with return type vector
+
+void primeFactorsBig(int n,map<int,int>& mp){
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				mp[p]++;
+				n/=p;
+			}else p++;
+		}else{
+			mp[n]++;
+			break;
+		}
+	}
+	return;
+}
+void primeFactorsBig(int n,vector<int>& v){
+	unordered_set<int> uns;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				uns.insert(p);
+				n/=p;
+			}else p++;
+		}else{
+			uns.insert(n);
+			break;
+		}
+	}
+	v.resize(uns.size());
+	int len=0;
+	for(auto num:uns) v[len++]=num;
+	sort(v.begin(), v.end());
+	return;
+}
+// isPrime
+bool isPrimeBig(int n){
+	if(n==1) return false;
+	int p=2;
+	while(true){
+		if(n>=p*p){
+			if(n%p==0){
+				return false;
+				n/=p;
+			}else p++;
+		}else{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+// sieve of erathosthens for prime
+const int N=1e6;
+vector<int> highestPrime(1e6,0);
+vector<int> lowestPrime(1e6,0);
+vector<int> primes;
+// vector<vector<int>> factorsOf(N);
+bitset <N> nonPrimeSet;
+void seive(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	
+	for(int num=2;num*num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num*num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+			}
+		}
+	}
+	
+}
+void seiveWithAllPrimes(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	
+	for(int num=2;num*num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num*num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+			}
+		}
+	}
+	for(int num=2;num<=N;num++){
+		if(0==nonPrimeSet[num]) primes.push_back(num);
+	}
+}
+void seiveWithHPLP(){
+	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	for(int num=2;num*num<=N;num++){
+		if(0==nonPrimeSet[num]){
+			for(int j=num*num;j<=N;j+=num){
+				nonPrimeSet[j]=1;
+				if(lowestPrime[j]==0) lowestPrime[j]=num;
+				highestPrime[j]=num;
+			}
+			highestPrime[num]=lowestPrime[num]=num;
+		}
+	}
+	
+}
+// void seiveWithAllFactors(){
+// 	nonPrimeSet[0]=nonPrimeSet[1]=1;
+	
+// 	for(int num=2;num<=N;num++){
+// 		factorsOf[num].push_back(1);
+// 		if(0==nonPrimeSet[num]){
+// 			for(int j=num*num;j<=N;j+=num){
+// 				factorsOf[j].push_back(num);
+// 				nonPrimeSet[j]=1;
+// 				if(lowestPrime[j]==0) lowestPrime[j]=num;
+// 				highestPrime[j]=num;
+// 			}primes.push_back(num);
+// 			highestPrime[num]=lowestPrime[num]=num;
+// 		}else{
+// 			for(int j=num+num;j<=N;j+=num){
+// 				factorsOf[j].push_back(num);
+// 			}
+// 		}
+// 		factorsOf[num].push_back(num);
+// 	}
+// }
+
+void segmentedSeive(int l,int r,vector<int>& segPrimes){
+	int limit=ceil(sqrt(r));
+	int sz=r-l+1;
+	vector<bool> bs(sz,0);
+	for(auto primeNum:primes){
+		if(primeNum<=limit){
+			int firstMultiple=(l/primeNum)*primeNum;
+			if(firstMultiple<l) firstMultiple+=l;
+			for(int num=max(firstMultiple,primeNum*primeNum);num<=r;num+=primeNum){
+				bs[num-l]=1;
+			}
+		}
+	}
+	for(int i=0;i<sz;i++){
+		if(bs[i]==0) segPrimes.push_back(i+l);
+	}
+}
+int rangePrimes(int l,int r){
+	if(r<=1e6){
+		int ctr=0;
+		for(int i=l;i<=r;i++){
+			if(nonPrimeSet[i]==0) ctr++;
+		}
+		return ctr;
+	}else{
+		vector<int> segPrimes;
+		segmentedSeive(l,r,segPrimes);
+		return segPrimes.size();
+	}
+}
+void rangePrimes(int l,int r,vector<int>& segPrimes){
+	if(r<=1e6){
+		for(int i=l;i<=r;i++){
+			if(nonPrimeSet[i]==0) segPrimes.push_back(i);
+		}
+		return; 
+	}
+	return segmentedSeive(l,r,segPrimes);
+}
+bool isPrime(int n){
+	if(n>1e6) return isPrimeBig(n);
+	return 0==nonPrimeSet[n];
+}
+void primeFactors(int n,vector<int>&v){
+	if(n<2) return;
+	if(n>1e6) return primeFactorsBig(n,v);
+	while(n>1){
+		int prime_factor=highestPrime[n];
+		while(n%prime_factor==0){
+			n/=prime_factor;
+			
+		}
+		v.push_back(prime_factor);
+	}
+}
+void primeFactors(int n,map<int,int>& mp){
+	if(n<2) return;
+	if(n>1e6) return primeFactorsBig(n,mp);
+	while(n>1){
+		int prime_factor=highestPrime[n];
+		while(n%prime_factor==0){
+			n/=prime_factor;
+			mp[prime_factor]++;
+		}
+	}
+}
+
+int primeFactors(int n){
+	if(n<2) return 0;
+	if(n>1e6) return primeFactorsBig(n);
+	int noOfPrimeFactors=0;
+	while(n>1){
+		int prime_factor=highestPrime[n];
+		while(n%prime_factor==0){
+			n/=prime_factor;
+			noOfPrimeFactors++;
+		}
+	}
+	return noOfPrimeFactors;
+}
+// roundOf
+int roundOf(int n){
+	return (int)pow(2,floor(log2(n)));
+}
+// string and pattern mathcing
+// string -- // z algorithm
+void createZ(string s,int z[]){
+	int n=s.size();
+	int l,r,k;
+	l=r=0;
+	z[0]=0;
+	for(int i=1;i<n;i++){
+		if(i>r){
+			l=r=i;
+			while(r<n && s[r]==s[r-l]){
+				r++;
+			}
+			z[i]=r-l;
+			r--;
+		}else{
+			k=i-l;
+			if(z[k]<r-i+1){
+				z[i]=z[k];
+			}else{
+				l=i;
+				while(r<n && s[r-l]==s[r]){
+					r++;
+				}
+				z[i]=r-l;
+				r--;
+			}
+		}
+	}
+}
+void zSearch(string text,string pattern,vector<int>& match){
+	string concat=pattern+"$"+text;
+	int sz=concat.length();
+	int psize=pattern.size();
+	int z[sz];
+	createZ(concat,z);
+	for(int i=0;i<sz;i++){
+		if(z[i]==psize){
+			match.push_back(i-psize-1);
+		}
+	}
+	return;
+}
+
+bool stringCompare(string &s,int ss,string &r,int rs,int len){
+	if(ss+len>s.size() || rs+len>r.size()) return false;
+	for(int i=0;i<len;i++,ss++,rs++){
+		if(s[ss]!=r[rs]) return false;
+	}
+	return true;
+}
+void rabin(string s,string pattern,vector<int>&match){
+	int sz=s.size();
+	int pz=pattern.size();
+	if(pz>sz) return;
+	int primeNum=3;
+	int hashVal=0;
+	int patternHash=0;
+	for(int i=0;i<pz;i++){
+		patternHash+=((int)(pattern[i]-97 + 1))*pow(primeNum,i);
+	}
+	
+	// cout<<"patternHash :"<<patternHash<<endl;
+	for(int i=0;i<pz;i++){
+		hashVal+=((int)(s[i]-97 + 1))*pow(primeNum,i);
+	}
+	if(hashVal==patternHash) match.push_back(0);
+	for(int i=pz;i<sz;i++){
+		
+		int k=i-pz;
+		hashVal-=(int)(s[k]-97+1);
+		hashVal/=3;
+		hashVal+=((int)(s[i]-97+1))*pow(primeNum,pz-1);
+		if(hashVal==patternHash && (stringCompare(s,k+1,pattern,0,pz))) match.push_back(k+1);
+	}
+}
+void prefix_function(string s,vector<int>& pf){
+	int n=s.size();
+	pf.resize(n);
+	pf[0]=0;
+	for(int i=1;i<n;i++){
+		int j=pf[i-1];
+		while(j>0 && s[i]!=s[j]){
+			j=pf[j-1];
+		}
+		if(s[i]==s[j]){
+			j++;
+		}
+		pf[i]=j;
+	}
+	// Time complexity : O(n)
+}
+void kmp(string s,string pattern,vector<int>&match){
+	int pz=pattern.size();
+	int pos(-1),i(0),j(0);
+	vector<int> pf;
+	prefix_function(pattern,pf);
+	for(auto num:pf) cout<<num<<" ";cout<<endl;
+	while(i<s.size()){
+		if(s[i]==pattern[j]){
+			j++;
+			i++;
+		}else{
+			if(j!=0){
+				j=pf[j-1];
+			}else i++;
+		}
+		if(j==pattern.size()){
+			match.push_back(i-pz);
+		}
+	}
+}
+void reject(){
+	cout<<-1<<endl;
+	return;
+}
+void freqscan(int b[],int n,unordered_map<int,int>& hash){
+	for(int i=0;i<n;i++){
+		cin>>b[i];
+		hash[b[i]]++;
+	}
+}
+class sparseTable{
+	int *arr;
+	int n;
+	vector<vector<int>> table;
+	int col=0;
+	public: 
+	void buildTable(){
+		for(int i=0;i<n;i++) table[i][0]=arr[i];
+		for(int j=1;j<col;j++){
+			for(int i=0;i+(1<<j)-1<n;i++){
+				table[i][j]=min(table[i][j-1],table[i+(1<<(j-1))][j-1]);
+			}
+		}
+	}
+	sparseTable(int *arr,int n){
+		this->arr=arr;
+		this->n=n;
+		col=1+log2(n);
+		table=*new vector<vector<int>>(n,*new vector<int>(col));
+		this->buildTable();
+	}
+	int getRangeMin(int l,int r){
+		int diff=r-l+1;
+		int j=log2(diff);
+		return min(table[l][j],table[r-(1<<j)+1][j]);
+	}
+	void print(){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<col;j++){
+				cout<<table[i][j]<<" ";
+			}cout<<endl;
+		}
+	}
+};
+class Query{
+public: 
+	int index,l,r,sum;
+};
+class mosAlgo{
+public: 
+	
+	int *arr;
+	int n;
+	int len;
+	// vector<Query> que;
+	mosAlgo(int *arr,int n){
+		this->arr=arr;
+		this->n=n;
+		this->len=sqrt(n);
+	}
+	void process(vector<Query>& que,vector<int>& v){
+		int q=que.size();
+		sort(que.begin(), que.end(),[&](Query&a,Query&b){
+			if((a.l)/len == (b.l)/len){
+				return a.r>b.r;
+			}
+			return a.l/len<b.l/len;
+		});
+		int currL=0,currR=-1,currSum=0;
+		for(int i=0;i<q;i++){
+			Query &query=que[i];
+			while(currL<query.l){
+				currSum-=arr[currL];
+				currL++;
+			}
+			while(currL>query.l){
+				currL--;
+				currSum+=arr[currL];
+			}
+			while(currR<query.r){
+				currR++;
+				currSum+=arr[currR];
+			}
+			while(currR>query.r){
+				currSum-=arr[currR];
+				currR--;
+			}
+			query.sum=currSum;
+			v[query.index]=currSum;
+		}
+	}
+};
+void solve(){
+	int a[] = { 7, 2, 3, 0, 5, 10, 3, 12, 18 };
+    int n = sizeof(a) / sizeof(a[0]);
+	mosAlgo mos=*new mosAlgo(a,n);
+	int q;
+	cin>>q;
+	vector<Query> que(q);
+	for(int i=0;i<q;i++){
+		que[i].index=i;
+		cin>>que[i].l>>que[i].r;
+		que[i].sum=0;
+	}
+	vector<int> ans(q);
+	mos.process(que,ans);
+	for(auto num:ans) cout<<num<<" ";cout<<endl;
+}
+signed main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+	// seive();
+	// seiveWithHPLP();
+	// seiveWithAllFactors();
+	// seiveWithAllPrimes();
+	int t=1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+	return 0;
+}
