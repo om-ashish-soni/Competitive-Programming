404e1f635c9388c79cd817bf2795ed2a31b59489
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-01-30 11:41:52 +0530

    Update starter.cpp

--- starterTemplate/starter.cpp
+++ starterTemplate/starter.cpp
@@ -716,12 +716,60 @@ void kmp(string s,string pattern,vector<int>&match){
 		}
 	}
 }
+void reject(){
+	cout<<-1<<endl;
+	return;
+}
+void freqscan(int b[],int n,unordered_map<int,int>& hash){
+	for(int i=0;i<n;i++){
+		cin>>b[i];
+		hash[b[i]]++;
+	}
+}
+class sparseTable{
+	int *arr;
+	int n;
+	vector<vector<int>> table;
+	int col=0;
+	public: 
+	void buildTable(){
+		for(int i=0;i<n;i++) table[i][0]=arr[i];
+		for(int j=1;j<col;j++){
+			for(int i=0;i+(1<<j)-1<n;i++){
+				table[i][j]=min(table[i][j-1],table[i+(1<<(j-1))][j-1]);
+			}
+		}
+	}
+	sparseTable(int *arr,int n){
+		this->arr=arr;
+		this->n=n;
+		col=1+log2(n);
+		table=*new vector<vector<int>>(n,*new vector<int>(col));
+		this->buildTable();
+	}
+	int getRangeMin(int l,int r){
+		int diff=r-l+1;
+		int j=log2(diff);
+		return min(table[l][j],table[r-(1<<j)+1][j]);
+	}
+	void print(){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<col;j++){
+				cout<<table[i][j]<<" ";
+			}cout<<endl;
+		}
+	}
+};
+
 void solve(){
-	int arr[7]={1,2,3,4,5,6,7};
-	int n=7;
-	sqrtDecomp sd=*new sqrtDecomp(arr,n);
-	cout<<sd.getRangeSum(0,4)<<endl;
-    
+	int a[] = { 7, 2, 3, 0, 5, 10, 3, 12, 18 };
+    int n = sizeof(a) / sizeof(a[0]);
+	sparseTable st=*new sparseTable(a,n);
+	cout<<st.getRangeMin(0,4)<<endl;
+	cout<<st.getRangeMin(4,7)<<endl;
+	cout<<st.getRangeMin(7,8)<<endl;
+	st.print();
+	// print(arr,n);
 }
 
 signed main(){
