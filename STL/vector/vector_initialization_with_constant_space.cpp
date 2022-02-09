423d998aa580b8e3c9e4fbf514b1548833655788
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-02-09 15:06:59 +0530

    Update vector_initialization_with_constant_space.cpp

--- STL/vector/vector_initialization_with_constant_space.cpp
+++ STL/vector/vector_initialization_with_constant_space.cpp
@@ -1,2 +1,18 @@
 vector<string> vs = vector<string>(5);
 vector<int> vi{vector<int>(5,0)};
+
+// You can initialize the data members at the point of declaration, but not with () brackets:
+
+class Foo {
+    vector<string> name = vector<string>(5);
+    vector<int> val{vector<int>(5,0)};
+};
+
+// Before C++11, you need to declare them first, then initialize them e.g in a contructor
+
+class Foo {
+    vector<string> name;
+    vector<int> val;
+ public:
+  Foo() : name(5), val(5,0) {}
+};
