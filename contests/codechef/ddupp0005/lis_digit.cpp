1a1e864ae3d184a3abf967fd7cf51f8771adbc38
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-02-12 22:40:10 +0530

    Create lis_digit.cpp

---
+++ contests/codechef/ddupp0005/lis_digit.cpp
@@ -0,0 +1,30 @@
+// Problem link : https://www.codechef.com/problems/LISDIGIT
+// Solution : below code
+
+#include <bits/stdc++.h>
+#define int long long 
+#define ll long long
+#define endl "\n"
+
+/*
+
+ ___________________________________________________________
+| AUTHOR  : Om Ashish Soni                                  |
+| code    : C++14                                           |
+| IDE     : Sublime txt                                     | 
+|___________________________________________________________|
+
+*/
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num;cin>>num;cout<<num;
+    }cout<<endl;
+}
+int32_t main() {
+    int t;
+    for(cin>>t;t;t--) solve();
+    return 0;
+}
