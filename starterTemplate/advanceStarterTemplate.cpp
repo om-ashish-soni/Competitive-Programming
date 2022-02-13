d6d9ad197dc87b5da4a4d1ff948c8f534f16bb30
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-02-13 18:26:18 +0530

    Update advanceStarterTemplate.cpp

--- starterTemplate/advanceStarterTemplate.cpp
+++ starterTemplate/advanceStarterTemplate.cpp
@@ -1087,10 +1087,118 @@ class NCRefficient{
 		return ans;
 	}
 };
+class centroidDecomp{
+public:
+	int no_of_vertices;
+	vector<vector<int>> tree;
+	vector<vector<int>> centroidTree;
+	vector<int> subtreeSizeOf;
+	vector<bool> visited;
+	vector<bool> isCentroid;
+	int stop=0;
+	centroidDecomp(int N){
+		this->no_of_vertices=N+1;
+		tree.resize(no_of_vertices);
+		subtreeSizeOf.resize(no_of_vertices);
+		centroidTree.resize(no_of_vertices);
+		visited.resize(no_of_vertices);
+		isCentroid=*new vector<bool>(no_of_vertices,false);
+	}
+	
+	void DFS(int src,int& N){
+		// cout<<"dfs at : "<<src<<endl;
+		if(++stop>=100) exit(0);
+		visited[src]=true;
+		N += 1;
+		subtreeSizeOf[src]=1;
+		for(auto childNode:tree[src]){
+			if(!visited[childNode] && !isCentroid[childNode]){
+				DFS(childNode,N);
+				subtreeSizeOf[src]+=subtreeSizeOf[childNode];
+			}
+		}
+	}
+	int getCentroidUtil(int src,int N){
+		bool is_this_node_centroid=true;
+		visited[src]=true;
+		int heaviest_child=0;
+		for(auto childNode:tree[src]){
+			if(!visited[childNode] && !isCentroid[childNode]){
+				if(subtreeSizeOf[childNode]>N/2){
+					is_this_node_centroid=false;
+				}
+				if(heaviest_child==0 || subtreeSizeOf[childNode] > subtreeSizeOf[heaviest_child]){
+					heaviest_child=childNode;
+				}
+			}
+		}
+		if(is_this_node_centroid && (N-subtreeSizeOf[src] <= N/2)){
+			return src;
+		}
+		return getCentroidUtil(heaviest_child,N);
+	}
+	int getCentroid(int src){
+		this->clearVisitedAndSubtreeSizeOf();
+		int N=0;
+		DFS(src,N);
+		this->clearVisited();
+		int centroid=getCentroidUtil(src,N);
+		isCentroid[centroid]=true;
+		return centroid;
+	}
+	int decompose(int root){
+		int currCentroidNode=getCentroid(root);
+		for(auto childNodeOfCentroid:tree[currCentroidNode]){
+			if(!isCentroid[childNodeOfCentroid]){
+				int subCentroidNode=this->decompose(childNodeOfCentroid);
+				centroidTree[currCentroidNode].push_back(subCentroidNode);
+				centroidTree[subCentroidNode].push_back(currCentroidNode);
+			}
+		}
+		return currCentroidNode;
+	}
+	void addEdge (int u,int v){
+		tree[u].push_back(v);
+		tree[v].push_back(u);
+	}
+	void clearVisitedAndSubtreeSizeOf(){
+		for(int i=0;i<no_of_vertices;i++){
+			visited[i]=false;
+			subtreeSizeOf[i]=0;
+		}
+	}
+	void clearVisited(){
+		for(int i=0;i<no_of_vertices;i++){
+			visited[i]=false;
+		}
+	}
+	void print(){
+		for(int i=1;i<no_of_vertices;i++){
+			cout<<i<<"< : >";
+			for(auto node:centroidTree[i]) cout<<node<<" ";cout<<endl;
+		}
+	}
+};
 void solve(){
-	int n(15),r(4),mod(1e9+7);
-	NCRefficient ncr=*new NCRefficient(n,mod);
-	cout<<ncr.get(n,r);
+	int n(16);
+	centroidDecomp cd=*new centroidDecomp(n);
+	cd.addEdge(1, 4);
+    cd.addEdge(2, 4);
+    cd.addEdge(3, 4);
+    cd.addEdge(4, 5);
+    cd.addEdge(5, 6);
+    cd.addEdge(6, 7);
+    cd.addEdge(7, 8);
+    cd.addEdge(7, 9);
+    cd.addEdge(6, 10);
+    cd.addEdge(10, 11);
+    cd.addEdge(11, 12);
+    cd.addEdge(11, 13);
+    cd.addEdge(12, 14);
+    cd.addEdge(13, 15);
+    cd.addEdge(13, 16);
+    cd.decompose(1);
+	cd.print();
 	cout<<endl;
 }
 
