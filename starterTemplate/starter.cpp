7e46a4bda536e2c295817949de4dd908579847b0
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-02-13 14:12:21 +0530

    Update starter.cpp

--- starterTemplate/starter.cpp
+++ starterTemplate/starter.cpp
@@ -95,6 +95,10 @@ int power(long long x, unsigned int y, int p)
     } 
     return res; 
 } 
+
+int modSub(int a,int b,int mod){
+	return ((a%mod - b%mod + mod)%mod);
+}
 int modMul(int a,int b,int mod){
 	return ((a%mod)*(b%mod))%mod;
 }
@@ -760,16 +764,334 @@ class sparseTable{
 		}
 	}
 };
+class Query{
+public: 
+	int index,l,r,sum;
+};
+class mosAlgo{
+public: 
+	
+	int *arr;
+	int n;
+	int len;
+	// vector<Query> que;
+	mosAlgo(int *arr,int n){
+		this->arr=arr;
+		this->n=n;
+		this->len=sqrt(n);
+	}
+	void process(vector<Query>& que,vector<int>& v){
+		int q=que.size();
+		sort(que.begin(), que.end(),[&](Query&a,Query&b){
+			if((a.l)/len == (b.l)/len){
+				return a.r>b.r;
+			}
+			return a.l/len<b.l/len;
+		});
+		int currL=0,currR=-1,currSum=0;
+		for(int i=0;i<q;i++){
+			Query &query=que[i];
+			while(currL<query.l){
+				currSum-=arr[currL];
+				currL++;
+			}
+			while(currL>query.l){
+				currL--;
+				currSum+=arr[currL];
+			}
+			while(currR<query.r){
+				currR++;
+				currSum+=arr[currR];
+			}
+			while(currR>query.r){
+				currSum-=arr[currR];
+				currR--;
+			}
+			query.sum=currSum;
+			v[query.index]=currSum;
+		}
+	}
+};
+// longest palindromic substring , manachers' algo
+class Manacher{
+	string s;
+	int n;
+	vector<int> palCount;
+	int mxIndex=-1,mx=0;
+	string longestPalindrome;
+public: 
+	void buildString(string str){
+		for(auto c:str){
+			s+="$";
+			s+=c;
+		}
+		s+="$";
+	}
+	Manacher(string s){
+		this->n=s.size();
+		this->n=2*n+1;
+		this->buildString(s);
+		palCount=*new vector<int>(n,0);
+		this->process();
+	}
+	void process(){
+		int iter=0;
+		int start(0),end(0);
+		for(int i=0;i<n;){
+			while(start>0 && end<n-1 && s[start-1]==s[end+1]){
+				start--;
+				end++;
+			}
+			palCount[i]=end-start+1;
+			if(end==n-1) break;
+			int newCenter=end + ((i%2 == 0)?(1):(0));
+			for(int j=i+1;j<=end;j++){
+				if(s[i]=='$') continue;
+				palCount[j]=min(palCount[i-(j-i)],2*(end-j)+1);
+				if(j+palCount[i-(j-i)]/2 == end){
+					newCenter=j;
+					break;
+				}
+			}
+			i=newCenter;
+			end=i+palCount[i]/2;
+			start=i-palCount[i]/2;
+		}
+		for(int i=0;i<n;i++){
+			if(palCount[i] >mx){
+				mx=palCount[i];
+				mxIndex=i;
+			}
+		}
+		longestPalindrome=s.substr(mxIndex-mx/2,mx);
+		string dummy=longestPalindrome;
+		longestPalindrome="";
+		for(auto c:dummy){
+			if(c!='$') longestPalindrome+=c;
+		}
+	}
+	void print(){
+		for(int i=0;i<n;i++){
+			cout<<i<<" : "<<palCount[i]<<endl;
+		}
+	}
+	int getLen(){
+		return mx/2;
+	}
+	string getPal(){
+		return longestPalindrome;
+	}
 
+};
+bool isPalindrome(string s){
+	string rev=s;
+	reverse(s.begin(), s.end());
+	return s==rev;
+}
+
+class EulerTotient{
+	public : 
+	int n;
+	vector<int> totient;
+	EulerTotient(int N){
+		this->n=N+1;
+		totient.resize(n);
+		for(int i=0;i<n;i++){
+			totient[i]=i;
+		}
+		for(int i=2;i<n;i++){
+			if(totient[i]==i){
+				for(int j=2*i;j<n;j+=i){
+					totient[j]*=(i-1);
+					totient[j]/=i;
+				}
+				totient[i]=i-1;
+			}
+		}
+	}
+	void print(){
+		for(auto num:totient) cout<<num<<" ";cout<<endl;
+	}
+	inline int get(int index){
+		return totient.at(index);
+	}
+};
+class MatrixExp{
+	public : 
+	int n,row,col;
+	vector<vector<int>>& mat;
+	vector<vector<int>> resultMat;
+	// function to multiply matrices 
+	vector<vector<int>> matrixMultiply(vector<vector<int>>& mat1,vector<vector<int>>& mat2){
+		if(mat1[0].size() != mat2.size()){
+			cout<<"invalid matrix matrixMultiply"<<endl;
+			exit(0);
+		}
+		int myrow=(int)mat1.size(),mycol=(int)mat2[0].size(),comm=(int)mat1[0].size();
+		vector<vector<int>> mulMat((int)mat1.size(),vector<int>((int)mat2[0].size()));
+		for(int i=0;i<myrow;i++){
+			for(int j=0;j<mycol;j++){
+				mulMat[i][j]=0;
+				for(int k=0;k<comm;k++){
+					mulMat[i][j]+=mat1[i][k]*mat2[k][j];
+				}
+			}
+		}
+		// cout<<"mat1 : "<<endl;printMatrix(mat1);
+		// cout<<"mat2 : "<<endl;printMatrix(mat2);
+		// cout<<"res : "<<endl;printMatrix(mulMat);
+		return mulMat;
+	}
+	// function to make identitiy matrix 
+	void makeIdentityMatrix(vector<vector<int>> &identityMat){
+		identityMat.resize(row);
+		for(int i=0;i<row;i++){
+			identityMat[i]=*new vector<int>(row,0);
+			identityMat[i][i]=1;
+		}
+		return;
+	}
+	// function to exponentiate matrix 
+	vector<vector<int>> exponentiate(int power){
+		if(power==1){
+			return mat;
+		}
+		vector<vector<int>> mymat;
+		if(power==0){
+			this->makeIdentityMatrix(mymat);
+
+			return mymat;
+		}
+		mymat=this->exponentiate(power/2);
+		vector<vector<int>> ansmat=matrixMultiply(mymat,mymat);
+		if(power & 1){
+			ansmat=matrixMultiply(ansmat,mat);
+		}
+		// cout<<" power : "<<power<<endl;
+		// printMatrix(ansmat);
+		return ansmat;
+	}
+	// constructor
+	MatrixExp(int N,vector<vector<int>> &vmat):n(N),mat(vmat){
+		row=mat.size();
+		col=mat[0].size();
+		resultMat=this->exponentiate(n);
+	}
+	// overloaded constructor
+	MatrixExp(int N):n(N),mat(*new vector<vector<int>>{{1,1},{1,0}}){
+		row=mat.size();
+		col=mat[0].size();
+		resultMat=this->exponentiate(n);
+	}
+	// getter for resultmatrix
+	vector<vector<int>>& getResultMatrix(){
+		return this->resultMat;
+	}
+	// method to print result mat
+	void printMatrix(vector<vector<int>>& vmatrix){
+		for(auto &rowv:vmatrix){
+			for(auto elem:rowv) cout<<elem<<" ";cout<<endl;
+		}
+	}
+	void print(){
+		for(auto &rowv:resultMat){
+			for(auto elem:rowv) cout<<elem<<" ";cout<<endl;
+		}
+	}
+};
+class Fibonacci{
+	public :
+	int n;
+	vector<vector<int>> mymat{{1,1},{1,0}};
+	MatrixExp &me;
+	Fibonacci(int n):n(n),me(*new MatrixExp(n,mymat)){}
+	int get(){
+		if(n==0) return 0;
+		vector<vector<int>>& fibMatrix=me.getResultMatrix();
+		return fibMatrix[0][0];
+	}
+};
+
+class NCR{
+	public : 
+	int mod=-1;
+	int n,r,res=1;
+	void computeNcr(){
+		if(r>n){
+			res=0;
+			return;
+		}
+		for(int i=0;i<r;i++){
+			res*=(n-i);
+			res/=(i+1);
+		}
+	}
+	void computeNcrMod(){
+		if(r>n){
+			res=0;
+			return;
+		}
+		for(int i=0;i<r;i++){
+			res=modMul(res,n-i,mod);
+			res=modDiv(res,i+1,mod);
+		}
+	}
+	NCR(int n,int r):n(n),r(r){
+		if(r>n-r) r=n-r;
+		this->computeNcr();
+	}
+	NCR(int n,int r,int mod):n(n),r(r),mod(mod){
+		if(r>n-r) r=n-r;
+		this->computeNcrMod();
+	}
+	int get(){
+		return res;
+	}
+};
+
+class NCRefficient{
+	public : 
+	int n,mod,r;
+	vector<int> fact,invFact,invNum;
+	void calculateFactorial(){
+		fact[0]=fact[1]=1;
+		for(int i=2;i<=n;i++){
+			fact[i]=(fact[i-1]*i)%mod;
+		}
+		return;
+	}
+	void calculateInverseNum(){
+		invNum[0]=invNum[1]=1;
+		for(int i=2;i<=n;i++){
+			invNum[i]=invNum[mod % i]*(mod - mod/i)%mod;
+		}
+	}
+	void calculateInverseFactorial(){
+		invFact[0]=invFact[1]=1;
+		for(int i=2;i<=n;i++){
+			invFact[i]=(invNum[i]*invFact[i-1])%mod;
+		}		
+	}
+	NCRefficient(int n,int mod):n(n),mod(mod){
+		fact.resize(n+1);
+		invFact.resize(n+1);
+		invNum.resize(n+1);
+		this->calculateFactorial();
+		this->calculateInverseNum();
+		this->calculateInverseFactorial();
+	}
+	int get(int N,int r){
+		if(N>n) return 0;
+		if(r<0 || r>N) return 0;
+		int ans=(fact[N]*invFact[r])%mod *(invFact[N-r])%mod;
+		return ans;
+	}
+};
 void solve(){
-	int a[] = { 7, 2, 3, 0, 5, 10, 3, 12, 18 };
-    int n = sizeof(a) / sizeof(a[0]);
-	sparseTable st=*new sparseTable(a,n);
-	cout<<st.getRangeMin(0,4)<<endl;
-	cout<<st.getRangeMin(4,7)<<endl;
-	cout<<st.getRangeMin(7,8)<<endl;
-	st.print();
-	// print(arr,n);
+	int n(15),r(4),mod(1e9+7);
+	NCRefficient ncr=*new NCRefficient(n,mod);
+	cout<<ncr.get(n,r);
+	cout<<endl;
 }
 
 signed main(){
