60076720d52b77d87108156287b5745007e1995e
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-02-18 12:15:12 +0530

    Update advanceStarterTemplate.cpp

--- starterTemplate/advanceStarterTemplate.cpp
+++ starterTemplate/advanceStarterTemplate.cpp
@@ -318,6 +318,17 @@ class disjointSet{
         }
         return true;
     }
+    void giveConnectedComps(vector<vector<int>>& comps){
+    	for(auto &pr:ump){
+    		if(pr.first==0) continue;
+    		if(pr.second > 0){
+    			comps[pr.second].push_back(pr.first);
+    		}else{
+    			comps[pr.first].push_back(pr.first);
+    		}
+    	}
+    	return;
+    }
     void print(){
         for(auto &pr:ump){
             cout<<"{"<<pr.first<<"<="<<pr.second<<"}"<<endl;
@@ -1222,12 +1233,79 @@ public:
 		return true;
 	}
 };
+
+class Graph{
+	public : 
+	vector<vector<int>> graph;
+	vector<vector<int>> solutionColorVectors;
+	vector<int> colorOf;
+	int n;
+	int no_of_colors=0;
+	Graph(int n){
+		graph.resize(n+1);
+		colorOf.assign(n+1,0);
+		this->n=n;
+	}
+	void set_no_of_colors(int m){
+		this->no_of_colors=m;
+	}
+	void addEdge(int u,int v){
+		graph[u].push_back(v);
+	}
+	void printGraph(){
+		for(int i=1;i<=n;i++){
+			cout<<i<<" : ";
+			for(auto neb:graph[i]){
+				cout<<neb<<" ";
+			}cout<<endl;
+		}
+	}
+	void mcolor(int start){
+		if(start>n){
+			solutionColorVectors.push_back(colorOf);
+			return;
+		}
+		vector<bool> isValidColor(no_of_colors+1,true);
+		for(auto node:graph[start]){
+			isValidColor[colorOf[node]]=false;
+		}
+		for(int i=1;i<=no_of_colors;i++){
+			if(isValidColor[i]){
+				colorOf[start]=i;
+				this->mcolor(start+1);
+			}
+		}
+		colorOf[start]=0;
+		return;
+	}
+	void printSolutionColorVectors(){
+		for(auto & colorVector:solutionColorVectors){
+			for(int i=1;i<=n;i++){
+				cout<<colorVector[i]<<" ";
+			}
+			cout<<endl;
+		}
+	}
+
+};
 void solve(){
-	Trie &tr=*new Trie();
-	tr.insert("ssv");
-	tr.insert("datt");
-	tr.insert("nrs");
-	cout<<"ssv : "<<boolalpha<<tr.searchPrefix("ssv")<<endl;
+	int n,edges;
+	cin>>n>>edges;
+	int no_of_colors;
+	cin>>no_of_colors;
+	Graph g=*new Graph(n);
+	g.set_no_of_colors(no_of_colors);
+	for(int i=0;i<edges;i++){
+		int u,v;
+		cin>>u>>v;
+		g.addEdge(u,v);
+		g.addEdge(v,u);
+	}
+	cout<<"graph : "<<endl;
+	g.printGraph();
+	g.mcolor(1);
+	cout<<"solution : "<<endl;
+	g.printSolutionColorVectors();
 }
 signed main(){
 	
