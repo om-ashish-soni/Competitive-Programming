b129685552fe664aa5b23ad9e6ab93c6f5cd70a9
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-04-09 19:20:16 +0530

    Create binaryTreeToDll.py

---
+++ data structure contribution/TREE/binaryTreeToDll.py
@@ -0,0 +1,125 @@
+# problem link : https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1#
+# solution : below code
+#User function Template for python3
+
+'''
+class Node:
+    """ Class Node """
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+'''
+
+#Function to convert a binary tree to doubly linked list.
+def treetodll(root):
+    if root==None: return [None,None]
+    ls,le=treetodll(root.left)
+    rs,re=treetodll(root.right)
+    if le : root.left=le;le.right=root
+    if rs : root.right=rs;rs.left=root
+    cs=ls;ce=re
+    if cs==None:cs=root
+    if ce==None:ce=root
+    return [cs,ce]
+class Solution:
+    def bToDLL(self,root):
+        if root==None: return None
+        head,tail=treetodll(root)
+        return head
+        # do Code here
+
+
+#{ 
+#  Driver Code Starts
+#Initial template for Python
+
+from collections import deque
+class Node:
+    """ Class Node """
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+
+import sys            
+def printDLL(head): #Print util function to print Linked List
+    prev = None
+    sys.stdout.flush()
+    while(head != None):
+        print(head.data, end=" ")
+        prev=head
+        head=head.right
+    print('')
+    while(prev != None):
+        print(prev.data, end=" ")
+        prev=prev.left
+    print('')
+    
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        head = ob.bToDLL(root)
+        printDLL(head)
+# } Driver Code Ends
