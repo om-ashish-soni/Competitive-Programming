71d0e7cfb193adbf5ef22e98d13ca44b0305ee95
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-05-07 14:28:02 +0530

    Add files via upload

---
+++ local_machine_stuff/codeforces/121/G.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+void print(vector<pair<int,int>>& v){
+	for(auto &pr:v){
+		cout<<"{"<<pr.first<<","<<pr.second<<"}"<<endl;
+	}
+	cout<<endl;
+}
+void print(std::vector<int>& v){
+	for(auto &num:v) cout<<num<<" ";cout<<endl;
+}
+void print(std::vector<string>& v){
+	for(auto &num:v) cout<<num<<" ";cout<<endl;
+}
+void print(std::vector<std::vector<int>>& v,int n){
+	// cout<<v.size()<<endl;
+	for(int i=n-1;i>=0;i--){
+		print(v[i]);
+	}
+	// for(auto &row: v) print(row);
+	// for(int i=v.size()-1;i>=0;i--){
+	// 	std::vector<int>& row=v[i];
+	// 	print(row);
+	// }
+}
+int lcm(int a,int b){
+	return (a*b)/(__gcd(a,b));
+}
+int gcd(int a,int b){
+	return __gcd(a,b);
+}
+void print(bool b){
+	if(b) cout<<"YES";
+	else cout<<"NO";
+	cout<<endl;
+}
+void print(pair<int,int>& pr){
+	cout<<"{"<<pr.first<<","<<pr.second<<"} ";
+}
+int power(long long x, unsigned int y, int p) 
+{ 
+    int res = 1; 
+    x = x % p; // Update x if it is more than or 
+    if (x == 0) return 0; // In case x is divisible by p;
+    while (y > 0) { 
+        if (y & 1) res = (res*x) % p; 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    return res; 
+} 
+int modMul(int a,int b,int mod){
+	return ((a%mod)*(b%mod))%mod;
+}
+int modAdd(int a,int b,int mod){
+	return ((a%mod)+(b%mod))%mod;
+}
+void print(int *arr,int start,int end){
+	for(int i=start;i<=end;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+void print(int *arr,int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+
+void solve(){
+	
+}
+
+signed main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t=1;
+	scanf("%lld",&t);
+	while(t--){
+		solve();
+	}
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+	return 0;
+}
