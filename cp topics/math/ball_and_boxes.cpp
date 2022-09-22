4019b0dd7c27b215f330a4f7a28fff04cef9b02c
Om Ashishkumar Soni <86783496+om-ashish-soni@users.noreply.github.com>
2022-09-22 18:46:56 +0530

    Create ball_and_boxes.cpp

---
+++ cp topics/math/ball_and_boxes.cpp
@@ -0,0 +1,16 @@
+// problem link : https://www.codechef.com/submit/BALLBOX
+// solution : below code
+
+void solve(int t){
+	//shree ganeshay namah
+	int n;
+	read(n);
+	int k;
+	read(k);
+	YESNO1((n>=(k*(k+1))/2));
+	return;
+
+
+	// print answers here
+
+}
