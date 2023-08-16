f85a51044b1e6958f6d2fbf61d56ce354d251596
Om Ashishkumar Soni <oms279400@gmail.com>
2023-08-16 16:54:01 +0530

    refinining manachers algo

--- algo/algo-implementation/manachersAlgoToFindLongestPalindromicSubstr.cpp
+++ algo/algo-implementation/manachersAlgoToFindLongestPalindromicSubstr.cpp
@@ -1,77 +1,101 @@
 // longest palindromic substring , manachers' algo
-class Manacher{
+class Manacher
+{
 	string s;
 	int n;
 	vector<int> palCount;
-	int mxIndex=-1,mx=0;
+	vector<int> palStart;
+	int mxIndex = -1, mx = 0;
 	string longestPalindrome;
-public: 
-	void buildString(string str){
-		for(auto c:str){
-			s+="$";
-			s+=c;
+
+public:
+	void buildString(string str)
+	{
+		for (auto c : str)
+		{
+			s += "$";
+			s += c;
 		}
-		s+="$";
+		s += "$";
 	}
-	Manacher(string s){
-		this->n=s.size();
-		this->n=2*n+1;
+	Manacher(string s)
+	{
+		this->n = s.size();
+		this->n = 2 * n + 1;
 		this->buildString(s);
-		palCount=*new vector<int>(n,0);
+		palCount.assign(n, 0);
+		palStart.resize(n);
+		for (int i = 0; i < n; i++)
+		{
+			palStart[i] = i;
+		}
 		this->process();
 	}
-	void process(){
-		int iter=0;
-		int start(0),end(0);
-		for(int i=0;i<n;){
-			while(start>0 && end<n-1 && s[start-1]==s[end+1]){
+	void process()
+	{
+		int iter = 0;
+		int start(0), end(0);
+		for (int i = 0; i < n;)
+		{
+			while (start > 0 && end < n - 1 && s[start - 1] == s[end + 1])
+			{
 				start--;
 				end++;
 			}
-			palCount[i]=end-start+1;
-			if(end==n-1) break;
-			int newCenter=end + ((i%2 == 0)?(1):(0));
-			for(int j=i+1;j<=end;j++){
-				if(s[i]=='$') continue;
-				palCount[j]=min(palCount[i-(j-i)],2*(end-j)+1);
-				if(j+palCount[i-(j-i)]/2 == end){
-					newCenter=j;
+			palCount[i] = end - start + 1;
+			palStart[start] = max(palStart[start], end - start + 1);
+			if (end == n - 1)
+				break;
+			int newCenter = end + ((i % 2 == 0) ? (1) : (0));
+			for (int j = i + 1; j <= end; j++)
+			{
+				palCount[j] = min(palCount[i - (j - i)], 2 * (end - j) + 1);
+				if (j + palCount[i - (j - i)] / 2 == end)
+				{
+					newCenter = j;
 					break;
 				}
 			}
-			i=newCenter;
-			end=i+palCount[i]/2;
-			start=i-palCount[i]/2;
+			i = newCenter;
+			end = i + palCount[i] / 2;
+			start = i - palCount[i] / 2;
 		}
-		for(int i=0;i<n;i++){
-			if(palCount[i] >mx){
-				mx=palCount[i];
-				mxIndex=i;
+		for (int i = 0; i < n; i++)
+		{
+			if (palCount[i] > mx)
+			{
+				mx = palCount[i];
+				mxIndex = i;
 			}
 		}
-		longestPalindrome=s.substr(mxIndex-mx/2,mx);
-		string dummy=longestPalindrome;
-		longestPalindrome="";
-		for(auto c:dummy){
-			if(c!='$') longestPalindrome+=c;
+		longestPalindrome = s.substr(mxIndex - mx / 2, mx);
+		string dummy = longestPalindrome;
+		longestPalindrome = "";
+		for (auto c : dummy)
+		{
+			if (c != '$')
+				longestPalindrome += c;
 		}
 	}
-	void print(){
-		for(int i=0;i<n;i++){
-			cout<<i<<" : "<<palCount[i]<<endl;
+	void print()
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << i << " , " << s[i] << " : " << palCount[i] << endl;
 		}
 	}
-	int getLen(){
-		return mx/2;
+	int getLen()
+	{
+		return mx / 2;
 	}
-	string getPal(){
+	string getPal()
+	{
 		return longestPalindrome;
 	}
+	string getNonPalSuffix()
+	{
+		// for(int i=0;i<n;i++) cout<<palStart[i]<<" ";
+		// cout<<endl;
+		return s.substr(palStart[0]);
+	}
 };
-void solve(){
-	string s="cabbacc";
-	cout<<s.size()<<endl;
-	Manacher mnc=*new Manacher(s);
-	cout<<mnc.getLen()<<endl;
-	cout<<mnc.getPal()<<endl;
-}
